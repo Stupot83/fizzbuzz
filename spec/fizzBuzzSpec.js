98442a4fa397a6f05e3ce8195c8b30c71b54e115
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-11-11 10:40:34 +0000

    :construction::white_check_mark: Handle multiples of 3 and all tests passing

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,18 @@
+describe('FizzBuzz', () => {
+
+  var fizzBuzz;
+
+  beforeEach(() => {
+    fizzBuzz = new FizzBuzz();
+  });
+
+  describe('multiples of 3', () => {
+    it('fizzes for 3', () => {
+      expect(fizzBuzz.play(3)).toEqual('Fizz');
+    });
+
+    it('fizzes for 6', () => {
+      expect(fizzBuzz.play(6)).toEqual('Fizz');
+    });
+  });
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,12 @@
+function FizzBuzz() {
+}
+
+FizzBuzz.prototype.play = function(number) {
+  if (this._isDivisibleBy(3, number)) {
+    return 'Fizz';
+  }
+};
+
+FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+  return number % divisor === 0;
+};
